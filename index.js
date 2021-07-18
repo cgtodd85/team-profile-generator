e3f2b991619b4134400582e1efe0b19d33b43606
Connor Todd <cgtodd85@gmail.com>
2021-07-18 12:57:09 -0400

    requirements for index js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const Employee = require("./lib/employee");
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const path = require("path");
+const managerQuestion = require("./src/manager-question");
+const engineerQuestion = require("./src/engineer-question");
+const internQuestions = require("./src/intern-question");
+const selectEmployee = require("./src/select-employee");
+
+const team = [];
