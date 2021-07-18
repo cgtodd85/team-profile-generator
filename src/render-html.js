73e8871113268dad6e2e7b6a96b8674f02836990
Connor Todd <cgtodd85@gmail.com>
2021-07-18 17:05:07 -0400

    updating render html

--- src/render-html.js
+++ src/render-html.js
@@ -4,7 +4,33 @@ const fs = require("fs");
 const path = require("path");
 
 function renderEmployeeCard(x, filePath) {
-  var HTML = ``;
+  var HTML = `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+        crossorigin="anonymous"
+      />
+      <link rel="stylesheet" type="text/css" href="style.css" />
+  
+      <script
+        src="https://kit.fontawesome.com/a4b238bebd.js"
+        crossorigin="anonymous"
+      ></script>
+      <title>Document</title>
+    </head>
+    <body>
+      <nav class="navbar-expand-lg bg-warning">
+        <h1>My Team</h1>
+        
+      </nav>
+      <!-- start the employee cards -->
+      <div class="wrapper-grid">`;
 
   x.forEach((Employee) => {
     HTML += ``;
@@ -21,7 +47,7 @@ function getExtra(employee) {
     case "Manager":
       return `
         <span><i class="fas fa-phone fa-lg"></i></span>
-        <span><a href="tel:+${employee.office}">${employee.office}</a></span>
+        <span><a href="Office: ${employee.office}">${employee.office}</a></span>
         `;
     case "Engineer":
       return `
