270ee2321d8b2e052c4f5d8c8ada012c3ac2f954
Connor Todd <cgtodd85@gmail.com>
2021-07-19 10:26:49 -0400

    refactor render html

--- src/render-html.js
+++ src/render-html.js
@@ -103,38 +103,6 @@ function renderEmployeeCard(team, filePath) {
   writeHTML(HTML, filePath);
 }
 
-function getExtra(employee) {
-  switch (employee.role) {
-    case "Manager":
-      return `
-        <span><i class="fas fa-phone fa-lg"></i></span>
-        <span><a href="Office: ${employee.office}">${employee.office}</a></span>
-        `;
-    case "Engineer":
-      return `
-        <span><i class="fab fa-github fa-lg"></i></span>
-        <span><a href="https://github.com/${employee.github}">github.com/${employee.github}</a></span>
-        `;
-    case "Intern":
-      return `
-        <span><i class="fas fa-university fa-lg"></i></span>
-        <span>${employee.school}</span>
-        `;
-  }
-}
-
-function getIcon(employee) {
-  switch (employee) {
-    case "Manager":
-      `<i class="fas fa-bullhorn"></i>`;
-      return;
-    case "Engineer":
-      return `<h3>${employee.role} <i class="fas fa-laptop-code fa-lg"></i></h3>`;
-    case "Intern":
-      return `<h3>${employee.role} <i class="fas fa-user-graduate fa-lg"></i></i></h3>`;
-  }
-}
-
 function writeHTML(html, filePath) {
   const dir = path.join(filePath, "dist");
   fs.mkdir(dir, { recursive: true }, (err) => {
